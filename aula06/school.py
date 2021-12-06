498188d3d72b7bd70893679070ee194182b26503
Francisco Ribeiro <15705566+rui2015@users.noreply.github.com>
2021-12-06 15:02:39 +0000

    Solved ex 3 (aula06)

--- aula06/school.py
+++ aula06/school.py
@@ -1,14 +1,33 @@
 # Complete o programa!
 
+# Índice Numero --> 0
+# Índice  Nome  --> 1
+# Índice Nota 1 --> 5
+# Índice Nota 2 --> 6
+# Índice Nota 3 --> 7
+
 # a)
 def loadFile(fname, lst):
-    ...
+    with open(fname) as f:
+        for line in f:
+            line = line.strip('\n')
+            if not line[0].isnumeric(): continue
+            lineSplit = line.split('\t')
+            tupleToAdd = (int(lineSplit[0]), lineSplit[1], float(lineSplit[5]), float(lineSplit[6]), float(lineSplit[7]))
+            lst.append(tupleToAdd)
+
     
 # b) Crie a função notaFinal aqui...
-...
+def notaFinal(reg):
+    nota1, nota2, nota3 = reg[2], reg[3], reg[4]
+    notaFinal = ( nota1 + nota2 + nota3 ) / 3
+    return notaFinal
 
 # c) Crie a função printPauta aqui...
-...
+def printPauta(lst):
+    print('{:>10} {:^50} {:>8}'.format("Nùmero", "Nome", "Nota"))
+    for reg in lst:
+        print('{:>10} {:^50} {:>8.1f}'.format(reg[0], reg[1], notaFinal(reg)))
 
 # d)
 def main():
@@ -19,10 +38,10 @@ def main():
     loadFile("school3.csv", lst)
     
     # ordenar a lista
-    ...
+    lst.sort()
     
     # mostrar a pauta
-    ...
+    printPauta(lst)
 
 
 # Call main function
