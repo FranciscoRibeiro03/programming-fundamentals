46a2abb324584cf599e3a602b0da2664468d3cdf
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2021-12-13 15:07:09 +0000

    Solved exercise 2 (aula07)

--- aula07/telefone.py
+++ aula07/telefone.py
@@ -6,9 +6,33 @@ def listContacts(dic):
     for num in dic:
         print("{:>12s} : {}".format(num, dic[num]))
 
-def filterPartName(contacts, partName):
+def addContact(dic):
+    number = input("Introduz o número que queres adicionar: ")
+    name = input("Introduz o nome correspondente ao número: ")
+    dic[number] = name
+    print("Adicionado o contacto {} com o número {}".format(name, number))
+
+def removeContact(dic):
+    number = input("Introduz o número que queres remover da lista: ")
+    name = dic[number]
+    dic.pop(number)
+    print("Removido o contacto {}, associado ao número {}".format(name, number))
+
+def searchNumber(dic):
+    number = input("Introduz o número que queres procurar: ")
+    if number in dic:
+        print("O contacto associado ao número {} é {}.".format(number, dic[number]))
+    else:
+        print("Nenhum contacto associado ao número {}".format(number))
+
+def filterPartName(contacts: dict, partName: str):
     """Returns a new dict with the contacts whose names contain partName."""
-    ...
+    newDict = {}
+    for contact in contacts:
+        name = contacts[contact]
+        if partName in name:
+            newDict[contact] = name
+    return newDict
 
 
 def menu():
@@ -44,6 +68,17 @@ def main():
         elif op == "L":
             print("Contactos:")
             listContacts(contactos)
+        elif op == "A":
+            addContact(contactos)
+        elif op == "R":
+            removeContact(contactos)
+        elif op == "N":
+            searchNumber(contactos)
+        elif op == "P":
+            pesquisa = input("Introduz o nome que queres procurar: ")
+            partDict = filterPartName(contactos, pesquisa)
+            print("Contactos com \"{}\":".format(pesquisa))
+            listContacts(partDict)
         else:
             print("Não implementado!")
     
