2adf4aa0a4f98deeefc3e126023c01f063b24a26
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2021-12-20 14:23:38 +0000

    Solved exercise 4.b (aula07)

--- aula07/aula07ex04.py
+++ aula07/aula07ex04.py
@@ -1,3 +1,13 @@
+def gerarJogos(listaEquipas):
+   assert len(listaEquipas) >= 2, "Requires two or more teams!"
+   lista = []
+   for eq1 in listaEquipas:
+      for eq2 in listaEquipas:
+         if eq1 == eq2:
+            continue
+         lista.append((eq1, eq2))
+   return lista
+
 def main():
     print("Introduz os nomes das equipas, uma de cada vez, e carrega no Enter quando terminares.")
     listaEquipas = []
@@ -6,6 +16,7 @@ def main():
         if equipasInput == '':
             break
         listaEquipas.append(equipasInput.strip())
-    print(listaEquipas)
+    listaJogos = gerarJogos(listaEquipas)
+    print(listaJogos)
 
 main()
