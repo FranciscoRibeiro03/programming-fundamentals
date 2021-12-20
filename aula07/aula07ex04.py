546f960a0e1e23ec6176b6bb069fd2f39c8ed5f4
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2021-12-20 14:50:02 +0000

    Solved exercise 4.c (aula07)

--- aula07/aula07ex04.py
+++ aula07/aula07ex04.py
@@ -8,15 +8,25 @@ def gerarJogos(listaEquipas):
          lista.append((eq1, eq2))
    return lista
 
+def perguntarResultados(listaJogos, listaResultados):
+    print("Introduz os resultados dos jogos separados por vírgulas, no formato 'resultado1,resultado2'")
+    print("Exemplo: '0,0'")
+    for jogo in listaJogos:
+        res1, res2 = input(f"Introduz o resultado do jogo {jogo}: ").split(',')
+        res1, res2 = int(res1), int(res2)
+        listaResultados[jogo] = (res1, res2)
+
 def main():
     print("Introduz os nomes das equipas, uma de cada vez, e carrega no Enter quando terminares.")
     listaEquipas = []
+    listaResultados = {}
     while True:
         equipasInput = input("Introduz o nome de uma equipa: ")
         if equipasInput == '':
             break
         listaEquipas.append(equipasInput.strip())
     listaJogos = gerarJogos(listaEquipas)
-    print(listaJogos)
+    perguntarResultados(listaJogos, listaResultados)
+    print(listaResultados)
 
 main()
