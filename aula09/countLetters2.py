e1b789838c18d4037b258d11b667ed93029008b2
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2022-01-17 14:28:06 +0000

    Solved ex01 (aula09)

---
+++ aula09/countLetters2.py
@@ -0,0 +1,22 @@
+from sys import argv
+
+def getDict():
+    lettersDict = {}
+    with open(argv[1], encoding="utf-8") as file:
+        for line in file:
+            for char in line:
+                if char.isalpha():
+                    char = char.lower()
+                    if char not in lettersDict:
+                        lettersDict[char] = 1
+                    else:
+                        lettersDict[char] += 1
+    return lettersDict
+
+def main():
+    assert len(argv) > 1, "Need at least one argument"
+    letras = getDict()
+    for key, value in sorted(letras.items(), reverse=True, key=lambda k:k[1]):
+        print("{} - {}".format(key, value))
+
+main()
